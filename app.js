c71b6df442b06848573e600b5570a837f0f71db2
Yash Satpute <yashsatpute2006@gmail.com>
2026-07-15 16:44:04 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("Hello World");
+
+// const arr = [1,2,3,4,5,6]
+// console.log(arr.map((num) => num * 5));
+// globalThis.console.log(module)
+// console.log();
+const add = require("./math")
+
+console.log(add(5,10)); //
